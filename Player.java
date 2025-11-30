ac004cdfcd4bb90324d01319c63017f793c8163c
Saurav Kumar Jha <165055204+SauravJha5@users.noreply.github.com>
2025-11-30 06:59:23 +0530

    Delete Player.java

--- Player.java
+++
@@ -1,9 +0,0 @@
-package com.sixtythreet0.player.core;
-
-import com.sixtythreet0.player.model.Message;
-
-public interface Player {
-    void start() throws Exception;
-    void shutdown() throws Exception;
-    void send(Message msg) throws Exception;
-}
